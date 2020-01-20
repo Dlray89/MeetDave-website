bba66ad24e54c52611ba688f755d6d29a6402d3f
DAP <58054581+Dlray89@users.noreply.github.com>
2020-01-20 17:58:34 -0500

    New Layout to my tech site

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-var controller = 
-new ScrollMagic.Controller("spacer");
+var controller = new ScrollMagic.Controller();
 
 $(".wrapper").each(function() {
-    var t1 = new TimelineMax();
-    var child = $(this).find(".child");
-    t1.to(child, 1, { y: -180, ease: Linear.easeNone});
+  var tl = new TimelineMax();
+  var child = $(this).find(".child");
+  tl.to(child, 1, { y: -180, ease: Linear.easeNone });
 
-    var scene = new ScrollMagic.Scene({
-        triggerElement: this,
-        triggerHook: 0.4,
-        duration: "100%"
-    })
-
-    .setTween(t1)
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.4,
+    duration: "100%"
+  })
+    .setTween(tl)
     .addIndicators({
-        colorTrigger: "black",
-        colorStart: "black",
-        colorEnd: "black",
-        indent: 10
+      colorTrigger: "black",
+      colorStart: "black",
+      colorEnd: "black",
+      indent: 10
     })
-    .addto(controller)
-})
+    .addTo(controller);
+});
